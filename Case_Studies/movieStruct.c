b19a9e2eb85f31330c9c212e82cb1b5be175f23b
Esmanur <esmakdgl61@gmail.com>
2026-09-10 17:22:51 +0300

    Update struct - enum movie practice

--- Case_Studies/movieStruct.c
+++ Case_Studies/movieStruct.c
@@ -1,18 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+enum Genre {
+	ACTION,
+	DRAMA,
+	SCI_FI,
+	COMEDY
+};
+
  struct movie{
 	int id;
 	char name[50];
-	char genre;
+	enum Genre genre;
 };
 
 typedef struct movie Movie;
 
 
+
+
 int main() {
 	Movie *movie1;
 	movie1=malloc(sizeof(Movie));
+	int choice;
+	
+	char *genreNames[] = {
+    "Action",
+    "Drama",
+    "Sci-Fi",
+    "Comedy"
+};
 	
 	if(movie1==NULL){
 		printf("Memory allocation failed!\n");
@@ -24,11 +41,19 @@ int main() {
 	getchar();
 	printf("Enter movie name:\n");
 	fgets(movie1->name, 50, stdin);
-	printf("Enter movie's genre (only a letter: a-action or d-drama):\n");
-	scanf(" %c", &movie1->genre);
+	
+	do{
+		printf("0-Action\n1-Drama\n2-Sci-Fi\n3-Comedy\n");
+	printf("Choose movie's genre: \n");
+	scanf("%d", &choice);
+	if(choice<0||choice>3) printf("Invalid number!\n");
+	}while(choice<0||choice>3);
+	
+	movie1->genre=choice;
 	
 	printf("==== MOVIE INFORMATION ====\n");
-	printf("- ID: %d\n- %s- %c\n", movie1->id, movie1->name, movie1->genre);
+	printf("- ID: %d\n- %s", movie1->id, movie1->name);
+	printf("- Genre: %s\n", genreNames[movie1->genre]);
 	
 	free(movie1);
 	
